61299a45705df1d99ad35c75b5950c4bd7b31440
Gleb Bychkov <bychkov-bgo@yandex.ru>
2023-02-13 00:37:46 +0300

    added errors handler

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,35 +1,60 @@
 const Card = require('../models/card');
+const {
+  ERROR_DATA,
+  ERROR_NOT_FOUND,
+  ERROR_INTERNAL,
+} = require('../errors/errors');
 
 const getAllCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' }));
 };
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_DATA).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 const deleteCard = (req, res) => {
   const { cardId } = req.params;
   Card.findByIdAndRemove(cardId)
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Документ не найден' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 
 const likeCard = (req, res) => {
   const { cardId } = req.params;
   Card.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Документ не найден' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 
 const dislikeCard = (req, res) => {
   const { cardId } = req.params;
   Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Документ не найден' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,62 @@
 const User = require('../models/user');
+const {
+  ERROR_DATA,
+  ERROR_NOT_FOUND,
+  ERROR_INTERNAL,
+} = require('../errors/errors');
 
 const getAllUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' }));
 };
 
 const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Документ не найден' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_DATA).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_DATA).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_DATA).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(ERROR_INTERNAL).send({ message: 'Произошла ошибка' });
+    });
 };
 
 module.exports = {
